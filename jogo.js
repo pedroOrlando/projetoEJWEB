892c1b555adee730783a38de4e9f7f1a28a12293
Unknown <pedro_orlando45@hotmail.com>
2018-07-12 10:26:52 -0300

    Mudanças no flipper
    quem ganha começa a partida.

--- jogo.js
+++ jogo.js
@@ -7,7 +7,7 @@ var cardCount;
 var p1;
 var p2;
 var placarPlayer1;
-var placarPlayer20;
+var placarPlayer2;
 var winningScore;
 var id;
 var pauser;
@@ -18,7 +18,9 @@ function init(){
 fullDeck = ["AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AE", "2E", "3E", "4E", "5E", "6E", "7E", "8E", "9E", "10E", "JE", "QE", "KE", "AO", "2O", "3O", "4O", "5O", "6O", "7O", "8O", "9O", "10O", "JO", "QO", "KO", "AP", "2P", "3P", "4P", "5P", "6P", "7P", "8P", "9P", "10P", "JP", "QP", "KP"];
 halfDeck1 = [];
 halfDeck2 = [];
-flipper = true;
+editName();
+setPontuação();
+flipper=false;
 cardCount = 0;
 placarPlayer1 = 0;
 placarPlayer2 = 0;
@@ -42,6 +44,12 @@ document.getElementById("yugi").style.width="120px";
 document.getElementById("kaiba").src="./images/kaiba.png";
 document.getElementById("kaiba").style.height="240px";
 document.getElementById("kaiba").style.width="160px";
+
+/*document.getElementById("pergunta").style.display="block";
+document.getElementById("button6").style.display="inline"; 
+document.getElementById("button7").style.display="inline"; */
+
+
 for (let i=0; i < document.getElementsByClassName("cartaImagem").length; i++){
 		document.getElementsByClassName("cartaImagem")[i].style.border = "40px solid pink";
 		document.getElementsByClassName("cartaImagem")[i].style.padding = "0px";
@@ -50,6 +58,10 @@ for (let i=0; i < document.getElementsByClassName("cartaImagem").length; i++){
 	}
 }
 //
+
+function choosenStarter(){
+	
+}
 
 function winner(i){
 	switch(i){
@@ -71,6 +83,7 @@ function winner(i){
 				document.getElementById("cardPic2").style.borderColor = "Red";
 				document.getElementById("button1").innerHTML="Reembaralhar";
 				document.getElementById("yugi").src="./images/winner"+(Math.floor(Math.random()*10)+1)+".webp";
+				flipper = true;
 				setTimeout(function(){
 					alert("Ponto pro jogador "+p1+"!");
 				}, 200)
@@ -99,6 +112,7 @@ function winner(i){
 				document.getElementById("cardPic1").style.borderColor = "Red";
 				document.getElementById("button1").innerHTML="Reembaralhar";
 				document.getElementById("kaiba").src="./images/winner"+(Math.floor(Math.random()*10)+1)+".webp";
+				flipper = false;
 				setTimeout(function(){
 					alert("Ponto pro jogador "+p2+"!");
 				}, 200)
@@ -128,23 +142,19 @@ function deckShuffleSplit (){
 }
 
 function cardTurnOver() {
-	
+	console.log(flipper);
 	if((placarPlayer1 < winningScore) && (placarPlayer2 < winningScore)){
 		if (cardCount < 52){
 			if(flipper){
 				document.getElementById("cardPic1").src = "./images/"+halfDeck1[0]+".png";		
 				spokenCards(cardCount);
 				halfDeck1.splice(0, 1);	
-				flipper = false;
 				cardCount++;
-				
 			}else{
 				document.getElementById("cardPic2").src = "./images/"+halfDeck2[0]+".png";		
 				spokenCards(cardCount);
 				halfDeck2.splice(0, 1);	
-				flipper = true;
 				cardCount++;
-				
 			}
 		}else if (cardCount===52){
 			document.getElementById("cardPic1").src="./images/giphy.webp";
@@ -160,6 +170,15 @@ function cardTurnOver() {
 	
 }
 
+function flipperSwitch(){
+	if (flipper){
+		flipper = false;
+	}
+	else {
+		flipper = true;
+	}
+}
+
 function spokenCards(i) {
 	switch (i){
 		
@@ -170,12 +189,14 @@ function spokenCards(i) {
 	
 	if(flipper){
 		document.getElementById("valorFalado1").innerHTML='"Às"';
+		flipperSwitch();
 		if(halfDeck1[0] === "AC" || halfDeck1[0] === "AO" || halfDeck1[0] === "AE" || halfDeck1[0] === "AP"){
 			winner(1);
 		}
 	}
 	else{
 		document.getElementById("valorFalado2").innerHTML='"Às"';
+		flipperSwitch();
 		if(halfDeck2[0] === "AC" || halfDeck2[0] === "AO" || halfDeck2[0] === "AE" || halfDeck2[0] === "AP"){
 			winner(2);
 		}
@@ -189,12 +210,14 @@ function spokenCards(i) {
 	
 	if(flipper){
 		document.getElementById("valorFalado1").innerHTML='"Dois"';
+		flipperSwitch();
 		if(halfDeck1[0] === "2C" || halfDeck1[0] === "2O" || halfDeck1[0] === "2E" || halfDeck1[0] === "2P"){
 			winner(1);
 		}
 	}
 	else{
 		document.getElementById("valorFalado2").innerHTML='"Dois"';
+		flipperSwitch();
 		if(halfDeck2[0] === "2C" || halfDeck2[0] === "2O" || halfDeck2[0] === "2E" || halfDeck2[0] === "2P"){
 			winner(2);
 		}
@@ -208,12 +231,14 @@ function spokenCards(i) {
 
 	if(flipper){
 		document.getElementById("valorFalado1").innerHTML='"Três"';
+		flipperSwitch();
 		if(halfDeck1[0] === "3C" || halfDeck1[0] === "3O" || halfDeck1[0] === "3E" || halfDeck1[0] === "3P"){
 			winner(1);
 		}
 	}
 	else{
 		document.getElementById("valorFalado2").innerHTML='"Três"';
+		flipperSwitch();
 		if(halfDeck2[0] === "3C" || halfDeck2[0] === "3O" || halfDeck2[0] === "3E" || halfDeck2[0] === "3P"){
 			winner(2);
 		}
@@ -227,12 +252,14 @@ function spokenCards(i) {
 
 	if(flipper){
 		document.getElementById("valorFalado1").innerHTML='"Quatro"';
+		flipperSwitch();
 		if(halfDeck1[0] === "4C" || halfDeck1[0] === "4O" || halfDeck1[0] === "4E" || halfDeck1[0] === "4P"){
 			winner(1);
 		}
 	}
 	else{
 		document.getElementById("valorFalado2").innerHTML='"Quatro"';
+		flipperSwitch();
 		if(halfDeck2[0] === "4C" || halfDeck2[0] === "4O" || halfDeck2[0] === "4E" || halfDeck2[0] === "4P"){
 			winner(2);
 		}
@@ -246,12 +273,14 @@ function spokenCards(i) {
 
 	if(flipper){
 		document.getElementById("valorFalado1").innerHTML='"Cinco"';
+		flipperSwitch();
 		if(halfDeck1[0] === "5C" || halfDeck1[0] === "5O" || halfDeck1[0] === "5E" || halfDeck1[0] === "5P"){
 			winner(1);
 		}
 	}
 	else{
 		document.getElementById("valorFalado2").innerHTML='"Cinco"';
+		flipperSwitch();
 		if(halfDeck2[0] === "5C" || halfDeck2[0] === "5O" || halfDeck2[0] === "5E" || halfDeck2[0] === "5P"){
 			winner(2);
 		}
@@ -265,12 +294,14 @@ function spokenCards(i) {
 
 	if(flipper){
 		document.getElementById("valorFalado1").innerHTML='"Seis"';
+		flipperSwitch();
 		if(halfDeck1[0] === "6C" || halfDeck1[0] === "6O" || halfDeck1[0] === "6E" || halfDeck1[0] === "6P"){
 			winner(1);
 		}
 	}
 	else{
 		document.getElementById("valorFalado2").innerHTML='"Seis"';
+		flipperSwitch();
 		if(halfDeck2[0] === "6C" || halfDeck2[0] === "6O" || halfDeck2[0] === "6E" || halfDeck2[0] === "6P"){
 			winner(2);
 		}
@@ -284,12 +315,14 @@ function spokenCards(i) {
 
 	if(flipper){
 		document.getElementById("valorFalado1").innerHTML='"Sete"';
+		flipperSwitch();
 		if(halfDeck1[0] === "7C" || halfDeck1[0] === "7O" || halfDeck1[0] === "7E" || halfDeck1[0] === "7P"){
 			winner(1);
 		}
 	}
 	else{
 		document.getElementById("valorFalado2").innerHTML='"Sete"';
+		flipperSwitch();
 		if(halfDeck2[0] === "7C" || halfDeck2[0] === "7O" || halfDeck2[0] === "7E" || halfDeck2[0] === "7P"){
 			winner(2);
 		}
@@ -303,12 +336,14 @@ function spokenCards(i) {
 
 	if(flipper){
 		document.getElementById("valorFalado1").innerHTML='"Oito"';
+		flipperSwitch();
 		if(halfDeck1[0] === "8C" || halfDeck1[0] === "8O" || halfDeck1[0] === "8E" || halfDeck1[0] === "8P"){
 			winner(1);
 		}
 	}
 	else{
 		document.getElementById("valorFalado2").innerHTML='"Oito"';
+		flipperSwitch();
 		if(halfDeck2[0] === "8C" || halfDeck2[0] === "8O" || halfDeck2[0] === "8E" || halfDeck2[0] === "8P"){
 			winner(2);
 		}
@@ -324,12 +359,14 @@ function spokenCards(i) {
 
 	if(flipper){
 		document.getElementById("valorFalado1").innerHTML='"Nove"';
+		flipperSwitch();
 		if(halfDeck1[0] === "9C" || halfDeck1[0] === "9O" || halfDeck1[0] === "9E" || halfDeck1[0] === "9P"){
 			winner(1);
 		}
 	}
 	else{
 		document.getElementById("valorFalado2").innerHTML='"Nove"';
+		flipperSwitch();
 		if(halfDeck2[0] === "9C" || halfDeck2[0] === "9O" || halfDeck2[0] === "9E" || halfDeck2[0] === "9P"){
 			winner(2);
 		}
@@ -343,12 +380,14 @@ function spokenCards(i) {
 
 	if(flipper){
 		document.getElementById("valorFalado1").innerHTML='"Dez"';
+		flipperSwitch();
 		if(halfDeck1[0] === "10C" || halfDeck1[0] === "10O" || halfDeck1[0] === "10E" || halfDeck1[0] === "10P"){
 			winner(1);
 		}
 	}
 	else{
 		document.getElementById("valorFalado2").innerHTML='"Dez"';
+		flipperSwitch();
 		if(halfDeck2[0] === "10C" || halfDeck2[0] === "10O" || halfDeck2[0] === "10E" || halfDeck2[0] === "10P"){
 			winner(2);
 		}
@@ -362,12 +401,14 @@ function spokenCards(i) {
 
 	if(flipper){
 		document.getElementById("valorFalado1").innerHTML='"Valete"';
+		flipperSwitch();
 		if(halfDeck1[0] === "JC" || halfDeck1[0] === "JO" || halfDeck1[0] === "JE" || halfDeck1[0] === "JP"){
 			winner(1);
 		}
 	}
 	else{
 		document.getElementById("valorFalado2").innerHTML='"Valete"';
+		flipperSwitch();
 		if(halfDeck2[0] === "JC" || halfDeck2[0] === "JO" || halfDeck2[0] === "JE" || halfDeck2[0] === "JP"){
 			winner(2);
 		}
@@ -381,6 +422,7 @@ function spokenCards(i) {
 
 	if(flipper){
 		document.getElementById("valorFalado1").innerHTML='"Dama"';
+		flipperSwitch();
 		if(halfDeck1[0] === "QC" || halfDeck1[0] === "QO" || halfDeck1[0] === "QE" || halfDeck1[0] === "QP"){
 			winner(1);
 			
@@ -388,6 +430,7 @@ function spokenCards(i) {
 	}
 	else{
 		document.getElementById("valorFalado2").innerHTML='"Dama"';
+		flipperSwitch();
 		if(halfDeck2[0] === "QC" || halfDeck2[0] === "QO" || halfDeck2[0] === "QE" || halfDeck2[0] === "QP"){
 			winner(2);
 		}
@@ -401,12 +444,14 @@ function spokenCards(i) {
 
 	if(flipper){
 		document.getElementById("valorFalado1").innerHTML='"Rei"';
+		flipperSwitch();
 		if(halfDeck1[0] === "KC" || halfDeck1[0] === "KO" || halfDeck1[0] === "KE" || halfDeck1[0] === "KP"){
 			winner(1);
 		}
 	}
 	else{
 		document.getElementById("valorFalado2").innerHTML='"Rei"';
+		flipperSwitch();
 		if(halfDeck2[0] === "KC" || halfDeck2[0] === "KO" || halfDeck2[0] === "KE" || halfDeck2[0] === "KP"){
 			winner(2);
 		}
@@ -451,7 +496,6 @@ function defaultValues(){
 	document.getElementById("valorFalado2").innerHTML = '"Fale até cansar..."';
 	document.getElementById("yugi").src="./images/yugi.png";
 	document.getElementById("kaiba").src="./images/kaiba.png";
-	flipper = true;
 }
 
 function resetScore(){
@@ -461,8 +505,6 @@ function resetScore(){
 		placarPlayer1=0;
 		placarPlayer2=0;
 		init();
-		editName();
-		setPontuação();
 	}
 }
 
@@ -476,8 +518,6 @@ function newMatch(){
 		placarPlayer1=0;
 		placarPlayer2=0;
 		init();
-		editName();
-		setPontuação();
 	}
 }
 
